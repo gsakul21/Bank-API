aa372d5173adebc89ea52a8bb00e5d2f28d45349
Akul Gokaram <akul@Akuls-MacBook-Pro.local>
2024-05-07 02:44:29 -0400

    Commented BankLedgerController.

--- src/main/java/dev/codescreen/controller/BankLedgerController.java
+++ src/main/java/dev/codescreen/controller/BankLedgerController.java
@@ -12,8 +12,18 @@ import dev.codescreen.schemas.AuthorizationRequest;
 import dev.codescreen.schemas.AuthorizationResponse;
 import dev.codescreen.schemas.LoadRequest;
 import dev.codescreen.schemas.LoadResponse;
-import dev.codescreen.schemas.ResponseCode;
 import dev.codescreen.service.BankLedgerService;
+
+/*
+ * 
+ * This is the a controller responsible for handling the /authorization
+ * and /load endpoints of our service. It correctly maps the endpoints to their 
+ * services responsible for dealing with the expected functionality of the endpoint.
+ * 
+ * For more detail as to what occurs for each endpoint, scroll further down to
+ * learn more.
+ * 
+ */
 
 @RestController
 @RequestMapping("/")
@@ -22,30 +32,63 @@ public class BankLedgerController {
     @Autowired
     private BankLedgerService bankLedgerService;
 
+    /**
+     * @param authorizationRequest
+     * @return ResponseEntity<AuthorizationResponse> or ResponseEntity<ServerError>
+     * 
+     * This is the function that maps the REST endpoint corresponding to the
+     * authorization of funds functionality of the service. It calls the controller 
+     * responsible for those features and returns either a AuthorizationResponse as specified or 
+     * a ServerError object detailing what went wrong.
+     */
     @PutMapping("/authorization")
     public ResponseEntity<?> authorization(@RequestBody AuthorizationRequest authorizationRequest)
     {
+
+        // Calls the service responsible for handling fund authorization
         Object authResp = bankLedgerService.authorize(authorizationRequest);
 
+        /*
+         * Checking to see if service returned an error or a valid response
+         * object, indicating success. Sending result as 201 (Created) or
+         * 400 (Bad Request) accordingly.
+         */
         if (authResp.getClass() == AuthorizationResponse.class)
         {
-            AuthorizationResponse response = (AuthorizationResponse) authResp;
-            if (response.getResponse() == ResponseCode.DECLINED)
-            {
-                return ResponseEntity.badRequest().body(response);
-            }
+            /* Written under assumption that 201 (Created) is indicating creation
+             * of event tracking this request, so it doesn't matter if the
+             * authorization request is declined or approved, an event is made.
+             */
 
+            AuthorizationResponse response = (AuthorizationResponse) authResp;
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
         }
 
         return ResponseEntity.badRequest().body(authResp);
     }
 
+    
+    /**
+     * @param loadRequest
+     * @return ResponseEntity<LoadResponse> or ResponseEntity<ServerError>
+     * 
+     * This is the function that maps the REST endpoint corresponding to the
+     * load funds functionality of the service. It calls the controller responsible
+     * for those features and returns either a LoadResponse as specified or 
+     * a ServerError object detailing what went wrong.
+     * 
+     */
     @PutMapping("/load")
     public ResponseEntity<?> load(@RequestBody LoadRequest loadRequest)
     {
+        // Calls the service responsible for loading funds to a user.  
         Object loadResp = bankLedgerService.load(loadRequest);
 
+        /*
+         * Checking to see if service returned an error or a valid response
+         * object, indicating success. Sending result as 201 (Created) or
+         * 400 (Bad Request) accordingly.
+         */
         if (loadResp.getClass() == LoadResponse.class)
         {
             LoadResponse response = (LoadResponse) loadResp;
