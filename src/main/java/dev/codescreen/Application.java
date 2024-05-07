7abb8b593e53fc7287f49134a20701bfac217bdc
Akul Gokaram <akul@Akuls-MacBook-Pro.local>
2024-05-07 16:15:20 -0400

    commented Application class.

--- src/main/java/dev/codescreen/Application.java
+++ src/main/java/dev/codescreen/Application.java
@@ -3,6 +3,10 @@ package dev.codescreen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * This class is responsible for starting the service. Instantiates as a
+ * Spring Boot application and runs it, starting up everything required.
+ */
 @SpringBootApplication
 public class Application {
 
