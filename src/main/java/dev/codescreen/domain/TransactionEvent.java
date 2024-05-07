ec744c29afce103c251a8458a638f1e6702b9dea
Akul Gokaram <akul@Akuls-MacBook-Pro.local>
2024-05-07 13:30:25 -0400

    Commented TransactionEvent.

--- src/main/java/dev/codescreen/domain/TransactionEvent.java
+++ src/main/java/dev/codescreen/domain/TransactionEvent.java
@@ -10,28 +10,62 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+/*
+ * This is the class that defines an event for the service, as per the
+ * event sourcing pattern. The class will store relevant information for
+ * what is being denoted a "Transaction", this is essentially any load request
+ * or authorization request to an account. It stores information relevant to the
+ * event such as timestamp, involved User, as well as actually amount details (
+ * currency, amount, type).
+ */
+
 @Entity
 public class TransactionEvent {
 
+    /*
+     * A unique identifier for each event such that they can all be stored
+     * in the SQLite DB.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // The involved user for the transaction event
     private String userId;
 
+    // Message ID passed in with request
     private String messageId;
 
+    /*
+     * Custom Enum to denote and keep track of what actually happened with the
+     * load or authorization call. See {@link TransactionStatus} for more
+     * information.
+     */
     @Enumerated(EnumType.STRING)
     private TransactionStatus transactionStatus;
 
+    /*
+     * As specified in schema, representing if this was a credit or debit
+     * transaction. See {@link DebitCredit} for more information.
+     */
     @Enumerated(EnumType.STRING)
     private DebitCredit debitOrCredit;
+
+    /*
+     * Specific details to the transaction, retrieved from Amount object. See
+     * {@link Amount} for more information.
+     */
 
     private String currency;
 
     private BigDecimal amount;
 
     private String timeOfEvent;
+
+    /*
+     * Default constructor required for Entity objects and actual constructor
+     * used in {@link BankLedgerService}.
+     */
 
     public TransactionEvent() {}
 
@@ -45,6 +79,8 @@ public class TransactionEvent {
         this.amount = new BigDecimal(amount);
         this.timeOfEvent = timeOfEvent;
     }
+
+    // Getters and setters for various fields of the event object.
 
     public Long getId() {
         return id;
